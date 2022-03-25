5bf827339b8907442ba58811ef8f710edb0bb99b
Pignataro67 <Pignataro67@gmail.com>
2022-03-25 10:03:51 -0700

    JS

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const bg = document.querySelector('.bg')
 let load = 0
 
 let int = setInterval(blurring, 30)
+
+function blurring() {
+  load++  
+    
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+  
